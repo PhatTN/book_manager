1c043f83aeacdb0ee8c65ebe61f5e82c91e9ab04
PhatTN <phattnse90070@gmail.com>
2018-12-14 10:04:37 +0700

    Fix failed tests

--- src/common/router.ts
+++ src/common/router.ts
@@ -11,7 +11,7 @@ export abstract class Router {
   public abstract routes(): IMiddleware;
 
   protected buildSuccessArrayBodyWithoutPaging<T>(data: T[]): SuccessArrayResponseWithoutPaging<T> {
-    return { dataa: data };
+    return { data };
   }
 
   protected handlePublicRoute(
@@ -28,5 +28,5 @@ interface SuccessResponse<T> {
 }
 
 interface SuccessArrayResponseWithoutPaging<T> {
-  dataa: T[];
+  data: T[];
 }
